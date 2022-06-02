444ec617c0ca5bd6ca7a12d522b8fe04e886d8a9
joanneks <joannekan.sh@gmail.com>
2022-06-02 08:20:25 +0000

    Added node_modules, hbs. Created 2 folders 01-basic-app, 03a-templates Coded using express

---
+++ 03a-templates/index.js
@@ -0,0 +1,44 @@
+// 1. require in express
+const express = require('express');
+
+// 1b. require in hbs
+const hbs = require('hbs');
+
+// 2. create the express application
+const app = express();
+
+// // 2b. tell Express that we want to use hbs as the template engine
+// app.set('view engine', 'hbs')
+// 
+// // 3. put in the routes
+// app.get('/', function(req,res){
+//     res.render("hello.hbs")
+// })
+
+// 2c. tell Express where to find static files
+// static files = browser javascript, images, css
+app.use(express.static('public')) // the 'public' refers to the 'public' folder
+
+// 3. put in the routes
+app.get('/', function(req,res){
+    res.render("index.hbs")
+})
+
+app.get('/hello/:firstname/:lastname', function(req,res){
+    let fname = req.params.firstname;
+    let lname = req.params.lastname;
+    // the second argument to render
+    // allows us to pass variables
+    // to the hbs file
+    // the key is the variable in the hbs file
+    // the value is the value for that variable
+    res.render('hello', {
+        'firstName': fname,
+        'lastName': lname
+    })
+})
+
+// 4. start server
+app.listen(3000, function(){
+    console.log("server started")
+})
