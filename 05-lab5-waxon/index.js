b7b84811d64b490adcdd05ad6a05a41df45c30ad
joanneks <joannekan.sh@gmail.com>
2022-06-03 06:08:59 +0000

    added handlebars

--- 05-lab5-waxon/index.js
+++ 05-lab5-waxon/index.js
@@ -17,8 +17,55 @@ app.get('/report',function(req,res){
     res.render('report');
 });
 
+app.get('/fault-list',function(req,res){
+    res.render('fault-list');
+});
+
+// app.get('/fruits',function(req,res){
+//     res.render('fruits',{
+//         'fruits':['apples', 'oranges', 'pears','mangosteens']
+//     })
+// });
+
+app.get('/fruits',function(req,res){
+    let dishes = [
+        {
+            'name':'Chicken Rice',
+            'calories':700,
+            
+        },
+        {
+            'name':'Roasted Duck Rice',
+            'calories':600,
+        },
+        {
+            'name':'Wanton Mee',
+            'calories':500,
+        },
+    ]
+    res.render('fruits',{
+        'fruits':['apples','oranges','pears','mangosteens'],
+        'dishes': dishes,
+        'favouriteDrink':''
+    })
+});
+
+
 app.listen(3000,function(){
     console.log('server started')
 });
 
+
+
+// register our customer handlebar helpers
+//-ifEquals-
+//the callback function has 3 arguments:
+// arg 2 is the data from the hbs
+// hbs.handlebars.registerHelper('ifEquals', function(arg1,arg2,arg3){
+    if(arg1==arg2){
+        options.fn(this);
+    }else{
+        options.inverse(this)
+    }
+})
 
