86ce2cde05462440e6891f4a50189aef4d2e0762
joanneks <joanne.kan@trentglobal.edu.sg>
2022-06-07 07:04:02 +0000

    Update and delete done for 07a-labget (movie)

--- 07a-labget/index.js
+++ 07a-labget/index.js
@@ -18,6 +18,7 @@ const BASE_API_URL = 'https://ckx-movies-api.herokuapp.com/';
 app.get('/',async function(req,res){
     let response = await axios.get(BASE_API_URL+"movies");
     let movies = response.data
+    // console.log(movies)
     res.render('allMovies',{
         'allMovies': response.data
     });
@@ -38,14 +39,51 @@ app.post('/create',async function(req,res){
     res.redirect('/');
 })
 
-// app.patch('/patch',async function(req,res){
-//     let update = {
-//         'title':req.body.title,
-//         'plot':req.body.plot
-//     }
-//     await axios.patch(BASE_API_URL+"movie/create",newMovie);
-//     res.redirect('/');
-// })
+app.get('/movie/edit/:movie_id',async function(req,res){
+    let movieId = req.params.movie_id
+    let url = BASE_API_URL + 'movie/' + movieId;
+    let response = await axios.get(url);
+    let movieSelected = response.data;
+    
+    res.render('update',{
+        'id':movieId,
+        'title':movieSelected.title,
+        'plot':movieSelected.plot
+    })
+})
+
+app.post('/movie/edit/:movie_id',async function(req,res){
+    let movieId = req.params.movie_id
+    let title = req.body.title;
+    let plot = req.body.plot;
+    let url = BASE_API_URL + "movie/" + movieId;
+    console.log(url);
+    let updateMovie = {
+        'id':movieId,
+        'title':title,
+        'plot':plot
+    }
+    await axios.patch(url,updateMovie);
+    res.redirect('/');
+});
+
+app.get('/movie/delete/:movie_id',async function(req,res){
+    let movieId = req.params.movie_id
+    let url = BASE_API_URL + "movie/" + movieId;
+    let response = await axios.get(url);
+    let movieSelected = response.data;
+    res.render('delete',{
+        'title':movieSelected.title,
+        'plot':movieSelected.plot
+    })
+});
+
+app.post('/movie/delete/:movie_id',async function(req,res){
+    let movieId = req.params.movie_id;
+    let url = BASE_API_URL + "movie/" + movieId;
+    await axios.delete(url);
+    res.redirect('/');
+});
 
 app.listen(3000,function(){
     console.log("server started");
